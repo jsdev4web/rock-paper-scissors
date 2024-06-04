b1203e1a33692e03172366144c16a47762e72e90
jsweb4dev <jsweb4dev@gmail.com>
2024-06-04 15:14:19 -0400

    games works up to step 6

--- script.js
+++ script.js
@@ -14,10 +14,81 @@ function getComputerChoice(){
     }
 };
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
-let getHumanChoice = (prompt("Choose now"));
-console.log(getHumanChoice);
+function getHumanChoice(){
+    return (prompt("Choose now"));
+}
 
 let humanScore = 0;
 let computerScore = 0;
+let rounds = 0;
+
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = getHumanChoice().toLowerCase();
+
+    computerChoice = getComputerChoice();
+
+    console.log("you choose " + humanChoice)
+    console.log("cpu choose " + computerChoice)
+    if (humanChoice === "rock" && computerChoice === "rock" ||
+        humanChoice === "paper" && computerChoice === "paper" ||
+        humanChoice === "scissors" && computerChoice === "scissors"){
+            console.log("human score: " + humanScore +
+                " cpu score: " + computerScore);
+            console.log( "draw")
+        }
+    else if (humanChoice === "rock" && computerChoice === "scissors" ||
+             humanChoice === "paper" && computerChoice === "rock" ||
+             humanChoice === "scissors" && computerChoice === "paper"){
+                humanScore++
+                console.log("human score: " + humanScore +
+                " cpu score: " + computerScore);
+                console.log( "you wins");
+        }
+    else if (humanChoice === "rock" && computerChoice === "paper" ||
+             humanChoice === "paper" && computerChoice === "scissors" ||
+             humanChoice === "scissors" && computerChoice === "rock"){
+                computerScore++
+                console.log("human score: " + humanScore +
+                " cpu score: " + computerScore);
+                console.log( "you lose");
+   }
+    else{
+        console.log( "invalid option" );
+   };
+
+};
+
+function winner(){
+    if(humanScore === computerScore){
+        return "No one wins"
+    }
+    else if(humanScore > computerScore){
+        return "you win"
+    }
+    else{
+        return "computer wins"
+    }
+}
+
+
+function playGame(){
+
+    console.log(rounds);
+
+    if (rounds !== 5){
+        rounds++
+        playRound()
+    }
+    else if ( rounds === 5){
+        console.log(winner())
+        return "game over "
+    }
+    playGame()
+};
+
+//playRound();
+
+playGame()
